66b2a55bb9eccd66d8c973fe3882f2bca0e6306c
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-01-20 11:54:12 +0100

    proj-html-vuejs header in progress

---
+++ main.js
@@ -0,0 +1,33 @@
+/* ADESSO MI CREO LA MIA LISTA DI OGGETTI DA RICHIAMARE */
+
+let app = new Vue({
+  el: "#app",
+  data: {
+    navbar: [
+      {
+        text: "HOME",
+      },
+      {
+        text: "SHOP",
+      },
+      {
+        text: "ABOUT",
+      },
+      {
+        text: "GALLERY",
+      },
+      {
+        text: "LOCATIONS",
+      },
+      {
+        text: "JOURNAL",
+      },
+      {
+        text: "CONTACT",
+      },
+      {
+        text: "MY ACCOUNT",
+      },
+    ],
+  },
+});
